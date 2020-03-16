0a6c3b4a6d6c7946a8f68b95c2ec9d31e38d4505
ahedin <andrew.hedin@lairdconnect.com>
2020-03-16 14:42:41 -0500

    Headers and layout in files needs standardizing
    Bug 16134

--- oob_demo/src/lte.c
+++ oob_demo/src/lte.c
@@ -1,6 +1,8 @@
-/* lte.c - LTE management
+/**
+ * @file lte.c
+ * @brief LTE management
  *
- * Copyright (c) 2019 Laird Connectivity
+ * Copyright (c) 2020 Laird Connectivity
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,6 +11,14 @@
 #define LOG_LEVEL LOG_LEVEL_DBG
 LOG_MODULE_REGISTER(oob_lte);
 
+#define LTE_LOG_ERR(...) LOG_ERR(__VA_ARGS__)
+#define LTE_LOG_WRN(...) LOG_WRN(__VA_ARGS__)
+#define LTE_LOG_INF(...) LOG_INF(__VA_ARGS__)
+#define LTE_LOG_DBG(...) LOG_DBG(__VA_ARGS__)
+
+/******************************************************************************/
+/* Includes                                                                   */
+/******************************************************************************/
 #include <zephyr.h>
 #include <net/net_if.h>
 #include <net/net_core.h>
@@ -24,23 +34,14 @@ LOG_MODULE_REGISTER(oob_lte);
 
 #include "lte.h"
 
-#define LTE_LOG_ERR(...) LOG_ERR(__VA_ARGS__)
-#define LTE_LOG_WRN(...) LOG_WRN(__VA_ARGS__)
-#define LTE_LOG_INF(...) LOG_INF(__VA_ARGS__)
-#define LTE_LOG_DBG(...) LOG_DBG(__VA_ARGS__)
-
+/******************************************************************************/
+/* Local Constant, Macro and Type Definitions                                 */
+/******************************************************************************/
 struct mgmt_events {
 	u32_t event;
 	net_mgmt_event_handler_t handler;
 	struct net_mgmt_event_callback cb;
 };
-
-static struct net_if *iface;
-static struct net_if_config *cfg;
-static struct mdm_receiver_context *mdm_rcvr;
-static struct dns_resolve_context *dns;
-static struct lte_status lteStatus;
-static lte_event_function_t lteCallbackFunction = NULL;
 
 static const struct led_blink_pattern NETWORK_SEARCH_LED_PATTERN = {
 	.on_time = DEFAULT_LED_ON_TIME_FOR_1_SECOND_BLINK,
@@ -48,6 +49,113 @@ static const struct led_blink_pattern NETWORK_SEARCH_LED_PATTERN = {
 	.repeat_count = REPEAT_INDEFINITELY
 };
 
+/******************************************************************************/
+/* Local Function Prototypes                                                  */
+/******************************************************************************/
+static void onLteEvent(enum lte_event event);
+
+static void iface_ready_evt_handler(struct net_mgmt_event_callback *cb,
+				    u32_t mgmt_event, struct net_if *iface);
+static void iface_down_evt_handler(struct net_mgmt_event_callback *cb,
+				   u32_t mgmt_event, struct net_if *iface);
+
+static void setup_iface_events(void);
+
+static void modemEventCallback(enum mdm_hl7800_event event, void *event_data);
+
+/******************************************************************************/
+/* Local Data Definitions                                                     */
+/******************************************************************************/
+static struct net_if *iface;
+static struct net_if_config *cfg;
+static struct mdm_receiver_context *mdm_rcvr;
+static struct dns_resolve_context *dns;
+static struct lte_status lteStatus;
+static lte_event_function_t lteCallbackFunction = NULL;
+
+static struct mgmt_events iface_events[] = {
+	{ .event = NET_EVENT_DNS_SERVER_ADD,
+	  .handler = iface_ready_evt_handler },
+	{ .event = NET_EVENT_IF_DOWN, .handler = iface_down_evt_handler },
+	{ 0 } /* The for loop below requires this extra location. */
+};
+
+/******************************************************************************/
+/* Global Function Definitions                                                */
+/******************************************************************************/
+void lteRegisterEventCallback(lte_event_function_t callback)
+{
+	lteCallbackFunction = callback;
+}
+
+int lteInit(void)
+{
+	int rc = LTE_ERR_NONE;
+	mdm_hl7800_register_event_callback(modemEventCallback);
+	setup_iface_events();
+
+	/* wait for network interface to be ready */
+	iface = net_if_get_default();
+	if (!iface) {
+		LTE_LOG_ERR("Could not get iface");
+		rc = LTE_ERR_NO_IFACE;
+		goto exit;
+	}
+
+	cfg = net_if_get_config(iface);
+	if (!cfg) {
+		LTE_LOG_ERR("Could not get iface config");
+		rc = LTE_ERR_IFACE_CFG;
+		goto exit;
+	}
+
+	dns = dns_resolve_get_default();
+	if (!dns) {
+		LTE_LOG_ERR("Could not get DNS context");
+		rc = LTE_ERR_DNS_CFG;
+		goto exit;
+	}
+
+	/* Get the modem receive context */
+	mdm_rcvr = mdm_receiver_context_from_id(0);
+	if (mdm_rcvr == NULL) {
+		LTE_LOG_ERR("Invalid modem receiver");
+		rc = LTE_ERR_MDM_CTX;
+		goto exit;
+	}
+
+	lteStatus.radio_version = mdm_rcvr->data_revision;
+	lteStatus.IMEI = mdm_rcvr->data_imei;
+	lteStatus.ICCID = (const char *)mdm_hl7800_get_iccid();
+	lteStatus.serialNumber = (const char *)mdm_hl7800_get_sn();
+	mdm_hl7800_generate_status_events();
+
+exit:
+	return rc;
+}
+
+bool lteIsReady(void)
+{
+	struct sockaddr_in *dnsAddr;
+
+	if (iface != NULL && cfg != NULL && &dns->servers[0] != NULL) {
+		dnsAddr = net_sin(&dns->servers[0].dns_server);
+		return net_if_is_up(iface) && cfg->ip.ipv4 &&
+		       !net_ipv4_is_addr_unspecified(&dnsAddr->sin_addr);
+	}
+
+	return false;
+}
+
+struct lte_status *lteGetStatus(void)
+{
+	mdm_hl7800_get_signal_quality(&lteStatus.rssi, &lteStatus.sinr);
+	return &lteStatus;
+}
+
+/******************************************************************************/
+/* Local Function Definitions                                                 */
+/******************************************************************************/
 static void onLteEvent(enum lte_event event)
 {
 	if (lteCallbackFunction != NULL) {
@@ -78,13 +186,6 @@ static void iface_down_evt_handler(struct net_mgmt_event_callback *cb,
 	led_turn_off(RED_LED3);
 	onLteEvent(LTE_EVT_DISCONNECTED);
 }
-
-static struct mgmt_events iface_events[] = {
-	{ .event = NET_EVENT_DNS_SERVER_ADD,
-	  .handler = iface_ready_evt_handler },
-	{ .event = NET_EVENT_IF_DOWN, .handler = iface_down_evt_handler },
-	{ 0 } // The for loop below requires this extra location.
-};
 
 static void setup_iface_events(void)
 {
@@ -171,74 +272,4 @@ static void modemEventCallback(enum mdm_hl7800_event event, void *event_data)
 		LOG_ERR("Unknown modem event");
 		break;
 	}
-}
-
-void lteRegisterEventCallback(lte_event_function_t callback)
-{
-	lteCallbackFunction = callback;
-}
-
-int lteInit(void)
-{
-	int rc = LTE_ERR_NONE;
-	mdm_hl7800_register_event_callback(modemEventCallback);
-	setup_iface_events();
-
-	/* wait for network interface to be ready */
-	iface = net_if_get_default();
-	if (!iface) {
-		LTE_LOG_ERR("Could not get iface");
-		rc = LTE_ERR_NO_IFACE;
-		goto exit;
-	}
-
-	cfg = net_if_get_config(iface);
-	if (!cfg) {
-		LTE_LOG_ERR("Could not get iface config");
-		rc = LTE_ERR_IFACE_CFG;
-		goto exit;
-	}
-
-	dns = dns_resolve_get_default();
-	if (!dns) {
-		LTE_LOG_ERR("Could not get DNS context");
-		rc = LTE_ERR_DNS_CFG;
-		goto exit;
-	}
-
-	/* Get the modem receive context */
-	mdm_rcvr = mdm_receiver_context_from_id(0);
-	if (mdm_rcvr == NULL) {
-		LTE_LOG_ERR("Invalid modem receiver");
-		rc = LTE_ERR_MDM_CTX;
-		goto exit;
-	}
-
-	lteStatus.radio_version = mdm_rcvr->data_revision;
-	lteStatus.IMEI = mdm_rcvr->data_imei;
-	lteStatus.ICCID = (const char *)mdm_hl7800_get_iccid();
-	lteStatus.serialNumber = (const char *)mdm_hl7800_get_sn();
-	mdm_hl7800_generate_status_events();
-
-exit:
-	return rc;
-}
-
-bool lteIsReady(void)
-{
-	struct sockaddr_in *dnsAddr;
-
-	if (iface != NULL && cfg != NULL && &dns->servers[0] != NULL) {
-		dnsAddr = net_sin(&dns->servers[0].dns_server);
-		return net_if_is_up(iface) && cfg->ip.ipv4 &&
-		       !net_ipv4_is_addr_unspecified(&dnsAddr->sin_addr);
-	}
-
-	return false;
-}
-
-struct lte_status *lteGetStatus(void)
-{
-	mdm_hl7800_get_signal_quality(&lteStatus.rssi, &lteStatus.sinr);
-	return &lteStatus;
 }
