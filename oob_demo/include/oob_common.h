de724dbd4104a8c771e4275d9a2b62d10b6580f3
Ryan Erickson <ryan.erickson@lairdconnect.com>
2020-03-24 15:28:37 -0500

    Bump version to 2.0.0
    Bump version to 2.0.0 to differentiate from GA1.  2.x will be the target version for GA2.

--- oob_demo/include/oob_common.h
+++ oob_demo/include/oob_common.h
@@ -21,7 +21,7 @@ extern "C" {
 /******************************************************************************/
 /* Application Firwware Version                                               */
 /******************************************************************************/
-#define APP_VERSION_MAJOR 1
+#define APP_VERSION_MAJOR 2
 #define APP_VERSION_MINOR 0
 #define APP_VERSION_PATCH 0
 #define APP_VERSION_STRING                                                     \
