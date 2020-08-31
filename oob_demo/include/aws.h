967ddd03e3ed3bad4e407a450139c29ff04975d1
ahedin <andrew.hedin@lairdconnect.com>
2020-08-31 08:22:34 -0500

    Add codedPhySupported for Bluegrass.
    Bug 17196
    When this value is present and true, then Bluegrass knows that the Pinnacle supports Coded PHY.

--- oob_demo/include/aws.h
+++ oob_demo/include/aws.h
@@ -89,6 +89,7 @@ struct shadow_persistent_values {
 	const char *IMEI;
 	const char *ICCID;
 	const char *radio_sn;
+	bool codedPhySupported;
 };
 
 struct shadow_state_reported {
@@ -112,6 +113,8 @@ static const struct json_obj_descr shadow_persistent_values_descr[] = {
 			    JSON_TOK_STRING),
 	JSON_OBJ_DESCR_PRIM(struct shadow_persistent_values, radio_sn,
 			    JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct shadow_persistent_values, codedPhySupported,
+			    JSON_TOK_TRUE),
 };
 
 static const struct json_obj_descr shadow_state_reported_descr[] = {
